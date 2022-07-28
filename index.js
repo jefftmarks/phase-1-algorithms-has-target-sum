38ec853f2cad3f4b0cfab983d0c14081ceff9be8
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-28 14:31:44 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let i = 0; i < array.length; i++) {
+    for(let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  } return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Big 0 notation: O(n^2)
 */
 
 /* 
